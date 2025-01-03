8f5a0b54595d762684feae506fcbb9e92cee3b99
Pepe222808 <129376609+Pepe222808@users.noreply.github.com>
2025-01-03 20:34:13 +0100

    change widgets location
    First commit to separate widgets from pages

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
 import '../api/user.dart';
+import '../widgets/waterWidget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -63,73 +64,7 @@ Container mainWidgets(context) {
   );
 }
 
-Container waterWidget(context) {
-  return Container(
-    margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-    width: 160,
-    height: 160,
-    padding: const EdgeInsets.all(10),
-    decoration: BoxDecoration(
-      boxShadow: [
-        BoxShadow(
-          color: Color(0xff1D1617).withOpacity(0.05),
-          blurRadius: 40,
-          spreadRadius: 0.0,
-        )
-      ],
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(10),
-      border: Border.all(color: Colors.blue, width: 1),
-    ),
-    child: Column(
-      children: [
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(
-              Icons.water_drop,
-              size: 30,
-              color: Colors.blue,
-            ),
-            Text('Water',
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)),
-          ],
-        ),
-        const SizedBox(height: 10),
-        // Chwilowo jest const potem zmienić jak będzie działało zapisywanie wody
-        const Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('1.5L/2L',
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold)),
-          ],
-        ),
-        const SizedBox(height: 15),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-              ),
-              onPressed: () {
-                print('Drink water');
-              },
-              child: const Text('Add water',
-                  style: TextStyle(color: Colors.white)),
-            ),
-          ],
-        )
-      ],
-    ),
-  );
-}
+
 
 Container frigdeWidget(context) {
   return Container(
